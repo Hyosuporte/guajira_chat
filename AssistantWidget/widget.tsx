1c90911f5b05ebd889bf2bb2051845d90db22cc1
Carlos Cuervo <cacuervo120@gmail.com>
2026-02-20 12:28:54 -0500

    update: add enviroment in file nextjs.config, add component assistant ui

--- AssistantWidget/widget.tsx
+++ AssistantWidget/widget.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import r2wc from 'react-to-webcomponent';
 import AssistantWidget from './AssistantWidget';
-import 'tailwindcss';
 
 const WidgetElement = r2wc(AssistantWidget, React, ReactDOM, {
   props: { apiUrl: 'string' },
