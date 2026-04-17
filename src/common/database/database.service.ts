1c9c8c80de90a2d75e2e42b3e0043830929817b8
Carlos Cuervo <cacuervo120@gmail.com>
2026-04-17 15:56:57 -0500

    :sparkles: Add module common/database

--- src/common/database/database.service.ts
+++ src/common/database/database.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Pool, QueryResult } from 'pg';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+  private pool: Pool;
+
+  onModuleInit() {
+    this.pool = new Pool({
+      host: process.env.DB_HOST,
+      port: 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      idleTimeoutMillis: 30000,
+      min: 1,
+      max: 10,
+    });
+  }
+
+  async query(text: string, params?: any[]): Promise<QueryResult> {
+    return this.pool.query(text, params);
+  }
+
+  async onModuleDestroy() {
+    await this.pool.end();
+  }
+}
