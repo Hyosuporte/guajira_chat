2eaf97d0b85d42c1201b69558c86297ceeec1046
Carlos Cuervo <cacuervo120@gmail.com>
2026-04-18 00:54:03 -0500

    :sparkles: Add module audit

--- src/audit/audit.controller.ts
+++ src/audit/audit.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { AuditService } from './audit.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auditoria')
 @Controller('audit')
-export class AuditController {}
+export class AuditController {
+  constructor(private readonly auditService: AuditService) {}
+
+  async executeAudit(@Body() body: { calId: number }) {
+    const erroresLey = await this.auditService.getValidadorLegal(body.calId);
+    const variaciones = await this.auditService.getDataSueldos(body.calId);
+    return { status: 'succes', data: { erroresLey, variaciones } };
+  }
+}
--- src/audit/audit.service.ts
+++ src/audit/audit.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/common/database/database.service';
 
 @Injectable()
-export class AuditService {}
+export class AuditService {
+  constructor(private readonly db: DatabaseService) {}
+
+  private readonly CONSTANTES_LEY = {};
+
+  async getValidadorLegal(calId: number) {
+    const querySubsidios = 'sian.ts';
+    const { rows } = await this.db.query(querySubsidios, [calId]);
+  }
+
+  async getDataSueldos(calIdActual: number) {
+    const query = `sian.ts`;
+    const { rows } = await this.db.query(query, [calIdActual]);
+    //Logica comparacion sueldos
+  }
+}
