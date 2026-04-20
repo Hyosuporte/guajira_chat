c27098bbb7f29c1de00b3a0890986809f468af75
Carlos Cuervo <cacuervo120@gmail.com>
2026-04-20 00:42:18 -0500

    🏗️ feat: configure main entry point (CORS, Dotenv, Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Api')
